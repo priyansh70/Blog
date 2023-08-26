1a4369c7f48e9269b7816ee6e9f649e188499204
priyansh70 <priyansh70890@gmail.com>
2023-08-26 23:19:45 +0530

    Update Responsiveness in Hero Section

--- src/pages/home/container/Hero.jsx
+++ src/pages/home/container/Hero.jsx
@@ -6,9 +6,9 @@ const Hero = () => {
   return (
     <section className='container mx-auto flex flex-col p-5 lg:flex-row'>
       <div className="mt-10 lg:w-1/2">
-        <h1 className='font-roboto text-3xl text-center font-bold text-dark-soft md:text-5xl lg:text-left lg:max-w-[540px]'>Read the most interesting articles</h1>
-        <p className='text-dark-light mt-4 text-center md:text-xl lg:text-left'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt fugiat debitis tempore a at vel, voluptates culpa praesentium nulla voluptatum?</p>
-        <div className="flex flex-col gap-y-2.5 mt-10 relative">
+        <h1 className='font-roboto text-3xl text-center font-bold text-dark-soft md:text-5xl lg:text-4xl xl:text-5xl lg:text-left lg:max-w-[540px]'>Read the most interesting articles</h1>
+        <p className='text-dark-light mt-4 text-center md:text-xl lg:text-base xl:text-xl lg:text-left'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt fugiat debitis tempore a at vel, voluptates culpa praesentium nulla voluptatum?</p>
+        <div className="flex flex-col gap-y-2.5 mt-10 lg:mt-6 xl:mt-10 relative">
           <div className="relative">
             <FiSearch className='absolute left-3 top-1/2 -translate-y-1/2 w-6 h-6 text-[#959EAD]' />
             <input
@@ -22,8 +22,8 @@ const Hero = () => {
           </button>
         </div>
         <div className="flex flex-col mt-4 lg:flex-row lg: lg:flex-nowrap lg:items-start lg:gap-x-4 lg:mt-7">
-          <span className='text-dark-light font-semibold italic mt-2 lg:mt-4'>Popular Tags:</span>
-          <ul className='flex flex-wrap gap-x-2.5 gap-y-2.5 mt-3'>
+          <span className='text-dark-light font-semibold italic mt-2 lg:text-sm xl:text-base lg:mt-4'>Popular Tags:</span>
+          <ul className='flex flex-wrap gap-x-2.5 gap-y-2.5 mt-3 lg:text-sm xl:text-base'>
             <li className='rounded-lg bg-primary bg-opacity-10 px-3 py-1.5 text-primary font-semibold'>Design</li>
             <li className='rounded-lg bg-primary bg-opacity-10 px-3 py-1.5 text-primary font-semibold'>User Experience</li>
             <li className='rounded-lg bg-primary bg-opacity-10 px-3 py-1.5 text-primary font-semibold'>User Interface</li>
