ed8afb243f9c027852678cd93078adb0fac75aec
priyansh70 <priyansh70890@gmail.com>
2023-08-27 00:28:17 +0530

    Add Article Section

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,9 +1,13 @@
 import Logo from "../assets/Logo.svg";
 import HeroImage from "../assets/HeroImage.svg"
+import PostImage from "../assets/posts/post1.jpg"
+import PostProfile from "../assets/posts/post-profile.svg";
 
 const images = {
     Logo,
     HeroImage,
+    PostImage,
+    PostProfile
 };
 
 export default images;
--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import MainLayout from "../../components/MainLayout"
 import Hero from './container/Hero'
-
+import Aricles from "./container/Articles";
 
 const HomePage = () => {
   return (
     <MainLayout>
       <Hero />
+      <Aricles />
     </MainLayout>
   )
 }
