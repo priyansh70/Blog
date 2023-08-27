6858c648181db2d3c11c17fe35d4f96cd401d1e6
priyansh70 <priyansh70890@gmail.com>
2023-08-27 12:37:54 +0530

    Add - CTA Image

--- src/constants/images.js
+++ src/constants/images.js
@@ -2,12 +2,15 @@ import Logo from "../assets/Logo.svg";
 import HeroImage from "../assets/HeroImage.svg"
 import PostImage from "../assets/posts/post1.jpg"
 import PostProfile from "../assets/posts/post-profile.svg";
+import CtaImage from "../assets/CtaImage.jpg";
+
 
 const images = {
     Logo,
     HeroImage,
     PostImage,
-    PostProfile
+    PostProfile,
+    CtaImage
 };
 
 export default images;
