038534c5ed01ae347572974e90dd2a428cc76fb1
priyansh70 <priyansh70890@gmail.com>
2023-08-27 12:38:20 +0530

    Add - CTA Section

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import MainLayout from "../../components/MainLayout"
 import Hero from './container/Hero'
 import Aricles from "./container/Articles";
+import CTA from './container/CTA';
 
 const HomePage = () => {
   return (
     <MainLayout>
       <Hero />
       <Aricles />
+      <CTA />
     </MainLayout>
   )
 }
